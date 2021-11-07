9bf06ff2119c2c88e03014aa57784f27b6f98e14
mgfiles <mgfiles@ncsu.edu>
2021-11-07 14:56:49 -0500

    continued to fix issues in admin pages

--- src/ui/userview/admin/AdminAddActivityType.java
+++ src/ui/userview/admin/AdminAddActivityType.java
@@ -27,65 +27,66 @@ public class AdminAddActivityType {
 		
 		//Used to loop back if invalid activity input
 		boolean validInput = false;
-	    
-		try {
-			//Class.forName("oracle.jdbc.OracleDriver");
-            PreparedStatement pstmt = null;
-            
-            while (!validInput) {
-            	
-        		System.out.print("Enter New Activity Name: ");
-        		String name = scan.nextLine();
-        		System.out.print("Enter New Activity Code: ");
-        		String code = scan.nextLine();
-    			
-    			System.out.println("\n1) Add Activity Type\n2) Go Back");
-    			System.out.print("\nSelect an Option: ");
-    			
-    			boolean selected = false;
-    			int selection = 0;
-    			
-    			//Validate user selection of menu
-    			while (!selected) {
-    				try {
-    					selection = Integer.parseInt(scan.nextLine());
-    					if (selection < 1 || selection > 2) {
-    						throw new InputMismatchException();
-    					}
-    					selected = true;
-    					
-    				} catch (InputMismatchException e) {
-    					UserInterface.invalidInput();
-    				}
-    			}
-    			
-    			// If user selected to add type, otherwise exit to menu above
-    			if (selection == 1) {
-    				pstmt = conn.prepareStatement("INSERT INTO ActivityCategories VALUES(?,?)");
-                    
-                    pstmt.clearParameters();
-                    pstmt.setString(1, code);
-                    pstmt.setString(2, name);
-                    
-                    int rows = pstmt.executeUpdate();
-                    if (rows < 1) {
-                    	throw new SQLException();
-                    }
-                    else {
-                    	validInput = true;
-                    	System.out.println("Activity Type Saved");
-                    }
-    			}
-    			else {
-    				// Get out of loop and return to calling class even though no submission
-    				validInput = true;
-    			}
-            }
-            
-            
-		} catch (Throwable e) {
-			System.out.println("Invalid Customer Information. Try Again.");
+		
+		while (!validInput) {
+			
+			try {
+				//Class.forName("oracle.jdbc.OracleDriver");
+	            PreparedStatement pstmt = null;
+	            	
+	    		System.out.print("Enter New Activity Name: ");
+	    		String name = scan.nextLine();
+	    		System.out.print("Enter New Activity Code: ");
+	    		String code = scan.nextLine();
+				
+				System.out.println("\n1) Add Activity Type\n2) Go Back");
+				System.out.print("\nSelect an Option: ");
+				
+				boolean selected = false;
+				int selection = 0;
+				
+				//Validate user selection of menu
+				while (!selected) {
+					try {
+						selection = Integer.parseInt(scan.nextLine());
+						if (selection < 1 || selection > 2) {
+							throw new InputMismatchException();
+						}
+						selected = true;
+						
+					} catch (InputMismatchException e) {
+						UserInterface.invalidInput();
+					}
+				}
+				
+				// If user selected to add type, otherwise exit to menu above
+				if (selection == 1) {
+					pstmt = conn.prepareStatement("INSERT INTO ActivityCategories(acId, acName) VALUES(?,?)");
+	                
+	                pstmt.clearParameters();
+	                pstmt.setString(1, code);
+	                pstmt.setString(2, name);
+	                
+	                int rows = pstmt.executeUpdate();
+	                if (rows < 1) {
+	                	throw new SQLException();
+	                }
+	                else {
+	                	validInput = true;
+	                	System.out.println("Activity Type Saved");
+	                }
+				}
+				else {
+					// Get out of loop and return to calling class even though no submission
+					validInput = true;
+				}
+	            
+	            
+			} catch (Throwable e) {
+				System.out.println("Invalid Activity Information. Try Again.");
+			}
 		}
+	    
 	}
 
 }
