ff9ff2ecbfe63ac1cce1c3abd0dabbb83d4151e9
Grey Files <mgfiles@engr-ras-205.eos.ncsu.edu>
2021-11-08 14:13:09 -0500

    fixed syntax error in sql setup

--- sql_setup/setup.sql
+++ sql_setup/setup.sql
@@ -234,18 +234,18 @@ END;
  * Assertion to ensure not username same between customers and brands
  */
 CREATE ASSERTION noOverlapCustomerBrand
-CHECK ( NOT EXISTS(SELECT C.username FROM Customers C, Brands B WHERE C.username = B.username))
+CHECK ( NOT EXISTS(SELECT C.username FROM Customers C, Brands B WHERE C.username = B.username));
 
 /*
  * Assertion to ensure not username same between customers and admins
  */
 CREATE ASSERTION noOverlapCustomerAdmin
-CHECK ( NOT EXISTS(SELECT C.username FROM Customers C, Admins A WHERE C.username = A.username))
+CHECK ( NOT EXISTS(SELECT C.username FROM Customers C, Admins A WHERE C.username = A.username));
 
 /*
  * Assertion to ensure not username same between customers and brands
  */
 CREATE ASSERTION noOverlapAdminBrand
-CHECK ( NOT EXISTS(SELECT A.username FROM Admins A, Brands B WHERE A.username = B.username))
+CHECK ( NOT EXISTS(SELECT A.username FROM Admins A, Brands B WHERE A.username = B.username));
 
 
