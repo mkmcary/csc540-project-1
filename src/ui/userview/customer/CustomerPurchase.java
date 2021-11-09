274cadfed5374e697b163fe7a04b5b4aa8bb1f87
mkmcary <mkmcary@gmail.com>
2021-11-08 19:29:39 -0500

    Updated customer reward activities to properly select rules from the database when calculating points to earn.

--- src/ui/userview/customer/CustomerPurchase.java
+++ src/ui/userview/customer/CustomerPurchase.java
@@ -156,7 +156,7 @@ public class CustomerPurchase {
 					allTimePoints = origPoints.getInt("alltimepoints");
 					tierNumber = origPoints.getInt("tierNumber");
 				}
-				
+			
 				points += pointsToEarn;
 				allTimePoints += pointsToEarn;
 				
@@ -179,7 +179,7 @@ public class CustomerPurchase {
 				// Delete a gift card if necessary
 				if (chosenCardId != -1) {
 					PreparedStatement deleteCard = conn.prepareStatement("DELETE FROM GiftCards WHERE id = ?");
-					deleteCard.setInt(0, chosenCardId);
+					deleteCard.setInt(1, chosenCardId);
 					deleteCard.executeUpdate();
 				}
 				
